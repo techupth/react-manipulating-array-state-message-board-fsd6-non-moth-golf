c7a78d413e0d5b979c8ecbf16b500662273ede30
fossi13 <fossi13@users.noreply.github.com>
2024-06-06 13:56:02 +0700

    fix: add value array  aad remove value array

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,54 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messageInput, setMessage] = useState("");
+  const [messageArr, setMessageArr] = useState([]);
+
+  const handleMessageInput = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const addMessage = () => {
+    const newMessage = [...messageArr];
+    newMessage.push(messageInput);
+    setMessageArr(newMessage);
+    setMessage("")
+  };
+
+  const removeMessage = (index) => {
+    const removeMessage = [...messageArr];
+    removeMessage.splice(index, 1);
+    setMessageArr(removeMessage)
+  }
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={messageInput}
+            onChange={handleMessageInput}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addMessage}>
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {messageArr.map((message, index) => {
+          
+          return (
+            <div className="message" key={index}>
+              <h1>{message}</h1>
+              <button className="delete-button" onClick={removeMessage}>x</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
